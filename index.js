1fd6eff746144bc18afaf380da46cd736ebe8fa8
Mike North <michael.l.north@gmail.com>
2016-03-12 00:28:17 +0800

    Rename some stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 
 module.exports = {
-  name: 'material-design-icons',
+  name: 'ember-material-design-icons-shim',
   included: function(app) {
     this._super.included(app);
     app.import(app.bowerDirectory + '/material-design-icons/iconfont/MaterialIcons-Regular.eot', { destDir: 'assets' });
