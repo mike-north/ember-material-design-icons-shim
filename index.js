250981dbba50817d0cc48f9651df2853c2e7e00b
Mike North <michael.l.north@gmail.com>
2016-04-22 00:38:35 +0200

    Update ember-cli, include iconfont as vendored files

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ module.exports = {
   name: 'ember-material-design-icons-shim',
   included: function(app) {
     this._super.included(app);
-    app.import(app.bowerDirectory + '/material-design-icons/iconfont/MaterialIcons-Regular.eot', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/material-design-icons/iconfont/MaterialIcons-Regular.ijmap', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/material-design-icons/iconfont/MaterialIcons-Regular.svg', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/material-design-icons/iconfont/MaterialIcons-Regular.ttf', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/material-design-icons/iconfont/MaterialIcons-Regular.woff', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/material-design-icons/iconfont/MaterialIcons-Regular.woff2', { destDir: 'assets' });
-    app.import(app.bowerDirectory + '/material-design-icons/iconfont/material-icons.css');
+    app.import('vendor/iconfont/MaterialIcons-Regular.eot', { destDir: 'assets' });
+    app.import('vendor/iconfont/MaterialIcons-Regular.ijmap', { destDir: 'assets' });
+    app.import('vendor/iconfont/MaterialIcons-Regular.svg', { destDir: 'assets' });
+    app.import('vendor/iconfont/MaterialIcons-Regular.ttf', { destDir: 'assets' });
+    app.import('vendor/iconfont/MaterialIcons-Regular.woff', { destDir: 'assets' });
+    app.import('vendor/iconfont/MaterialIcons-Regular.woff2', { destDir: 'assets' });
+    app.import('vendor/iconfont/material-icons.css');
   }
 };
