64a8fd57f6268afdbedc16844769ac495bb542ac
Mike North <michael.l.north@gmail.com>
2016-05-05 17:16:17 +0800

    Tests to validate that CSS has been added to asset pipeline

---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,13 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | index');
+
+test('visiting /index', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(find('.material-icons').css('font-family'), "'Material Icons'");
+    assert.equal(currentURL(), '/');
+  });
+});
